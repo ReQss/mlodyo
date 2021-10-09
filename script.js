fc50fdfb7da8ce77f001d3c0fb4381f7c6fc2026
ReQss <46198811+ReQss@users.noreply.github.com>
2021-10-09 23:43:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var lista=["https://player.twitch.tv/?channel=mamm0n&parent=reqss.github.io"
           ];
 function podmiana(t){document.getElementById("animo").src=lista[t]}function przypinka(){lista_strona=document.getElementById("lista").style.display,"none"==lista_strona?document.getElementById("lista").style.display="block":document.getElementById("lista").style.display="none",stream=document.getElementById("streamokno").style.display}function screenmax(){var t=document.getElementById("streamokno");"90%"!=t.style.height?(t.style.height="90%",t.style.width="250px"):(t.style.height="100%",t.style.width="320px")}
+
+
+
+function change1() {
+    var  change1 = document.getElementById("info").innerHTML = "Odcinek 1";
+}
