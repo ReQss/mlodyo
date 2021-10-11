5658ccb2497f663279679f0e20a4c2c2e11cb6b0
ReQss <46198811+ReQss@users.noreply.github.com>
2021-10-11 15:45:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,135 @@
-var lista=["https://player.twitch.tv/?channel=mamm0n&parent=reqss.github.io"
-          ];
-function podmiana(t){document.getElementById("animo").src=lista[t]}function przypinka(){lista_strona=document.getElementById("lista").style.display,"none"==lista_strona?document.getElementById("lista").style.display="block":document.getElementById("lista").style.display="none",stream=document.getElementById("streamokno").style.display}function screenmax(){var t=document.getElementById("streamokno");"90%"!=t.style.height?(t.style.height="90%",t.style.width="250px"):(t.style.height="100%",t.style.width="320px")}
+var lista = [
+    
+  
+  ];
 
-
+function podmiana(odcinek){
+    document.getElementById("animo").src= lista[odcinek];
+    
+    }
+    function przypinka(){
+        lista_strona = document.getElementById("lista").style.display;
+        
+        if (lista_strona == 'none'){
+            document.getElementById("lista").style.display = 'block'
+            }
+        else{
+            document.getElementById("lista").style.display = 'none';
+            }
+        stream = document.getElementById("streamokno").style.display;
+        /*if (stream == 'none'){
+            document.getElementById("streamokno").style.display = 'block'
+            }
+        else{
+            document.getElementById("streamokno").style.display = 'none';
+            }*/
+    }
+    function screenmax(){
+        console.log('x');
+        var sliderdisplay = document.getElementById('myRange');
+        
+         sliderdisplay.style.display = 'block';
+          
+          
+          }
+function ep(){
+    document.getElementById("animo").src= lista[0];
+}
 
 function change1() {
-    var  change1 = document.getElementById("info").innerHTML = "Odcinek 1";
+    var  change = document.getElementById("info").innerHTML = "Film1";
 }
 
 function change11() {
-    var  change1 = document.getElementById("info").innerHTML = "";
+    var  change = document.getElementById("info").innerHTML = "";
 }
+
+function change2() {
+    var  change = document.getElementById("info").innerHTML = "Film2";
+}
+
+function change22() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+function change3() {
+    var  change = document.getElementById("info").innerHTML = "Film3";
+}
+
+function change33() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+function change4() {
+    var  change = document.getElementById("info").innerHTML = "Film4";
+}
+
+function change44() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+function change5() {
+    var  change = document.getElementById("info").innerHTML = "Film5";
+}
+
+function change55() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+function change6() {
+    var  change = document.getElementById("info").innerHTML = "Film6";
+}
+function change66() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+
+function author() {
+    var  change = document.getElementById("info").innerHTML = "®";
+}
+function author2() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+function size() {
+    var  change = document.getElementById("info").innerHTML = "Rozmiar";
+}
+function size2() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+
+
+function options() {
+    var  change = document.getElementById("info").innerHTML = "Widok";
+}
+function options2() {
+    var  change = document.getElementById("info").innerHTML = "";
+}
+function flushed(){
+    var  change = document.getElementById("info").innerHTML = " &#128563";
+    console.log('x')
+}
+function flushed2(){
+    var  change = document.getElementById("info").innerHTML = "";
+  
+}
+function slider()
+{
+    var slider = document.getElementById('myRange').value;
+    var stream= document.getElementById('streamokno').style.width = slider+'px';
+    var stream= document.getElementById('streamokno').style.height = slider/3.2+'%';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
