8e58008e6c597eea0939c0554062916c201c8724
ReQss <46198811+ReQss@users.noreply.github.com>
2021-10-19 16:47:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var lista = [
     
   
   ];
-
 function podmiana(odcinek){
     document.getElementById("animo").src= lista[odcinek];
     
     }
+
+       //DOM1
     function przypinka(){
         lista_strona = document.getElementById("lista").style.display;
-        
         if (lista_strona == 'none'){
             document.getElementById("lista").style.display = 'block'
             }
@@ -17,102 +17,68 @@ function podmiana(odcinek){
             document.getElementById("lista").style.display = 'none';
             }
         stream = document.getElementById("streamokno").style.display;
-        /*if (stream == 'none'){
-            document.getElementById("streamokno").style.display = 'block'
-            }
-        else{
-            document.getElementById("streamokno").style.display = 'none';
-            }*/
+       
     }
     function screenmax(){
-        console.log('x');
         var sliderdisplay = document.getElementById('myRange');
-        
+        var sliderchat = document.getElementById('sliderchat');
+
+        if(sliderdisplay.style.display =='block'){
+         sliderdisplay.style.display = 'none';
+         sliderchat.style.display = 'none';
+        }
+         else {
          sliderdisplay.style.display = 'block';
-          
-          
+         sliderchat.style.display = 'block';
+         }
           }
+          //DOM2
 function ep(){
     document.getElementById("animo").src= lista[0];
 }
 
-function change1() {
-    var  change = document.getElementById("info").innerHTML = "Film1";
+function info(film) {
+    var  change = document.getElementById("info").innerHTML = "Film"+film;
 }
 
-function change11() {
-    var  change = document.getElementById("info").innerHTML = "";
-}
-
-function change2() {
-    var  change = document.getElementById("info").innerHTML = "Film2";
-}
-
-function change22() {
-    var  change = document.getElementById("info").innerHTML = "";
-}
-function change3() {
-    var  change = document.getElementById("info").innerHTML = "Film3";
-}
-
-function change33() {
-    var  change = document.getElementById("info").innerHTML = "";
-}
-function change4() {
-    var  change = document.getElementById("info").innerHTML = "Film4";
-}
-
-function change44() {
-    var  change = document.getElementById("info").innerHTML = "";
-}
-function change5() {
-    var  change = document.getElementById("info").innerHTML = "Film5";
-}
-
-function change55() {
-    var  change = document.getElementById("info").innerHTML = "";
-}
-function change6() {
-    var  change = document.getElementById("info").innerHTML = "Film6";
-}
-function change66() {
+function infoclear() {
     var  change = document.getElementById("info").innerHTML = "";
 }
 
 function author() {
     var  change = document.getElementById("info").innerHTML = "®";
 }
-function author2() {
-    var  change = document.getElementById("info").innerHTML = "";
-}
+
 function size() {
     var  change = document.getElementById("info").innerHTML = "Rozmiar";
-}
-function size2() {
-    var  change = document.getElementById("info").innerHTML = "";
 }
 
 
 function options() {
     var  change = document.getElementById("info").innerHTML = "Widok";
 }
-function options2() {
-    var  change = document.getElementById("info").innerHTML = "";
-}
+
 function flushed(){
     var  change = document.getElementById("info").innerHTML = " &#128563";
     console.log('x')
 }
-function flushed2(){
-    var  change = document.getElementById("info").innerHTML = "";
-  
-}
+
 function slider()
 {
     var slider = document.getElementById('myRange').value;
     var stream= document.getElementById('streamokno').style.width = slider+'px';
     var stream= document.getElementById('streamokno').style.height = slider/3.2+'%';
 }
+function sliderchat()
+{
+    var slider = document.getElementById('sliderchat').value;
+    var stream= document.getElementById('chat').style.width = slider+'px';
+    var anime=document.getElementById('film').style.width = `calc(100% - ${slider}px) `;
+
+   
+}
+
+
 
 
 
